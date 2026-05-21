708a6eec6bc5e93794627f3a23e7caa3149bebe2
unknown <aaravparikh86-beep>
2026-05-21 16:47:31 +0530

    pandas started

--- Libraries/NumPy/notes.py
+++ Libraries/NumPy/notes.py
@@ -1,1 +1,10 @@
-# pip intall NumPy to install NumPy
+# pip intall NumPy 
+# to insatal numpy in github
+
+
+# pip freeze
+# to see all the available libraries
+
+
+# import numpy as np
+# to import numpy in code
